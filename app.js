6df8e62fb9ea4b5532bbabb31362a85f3d3218f4
Abner Patrick D. Padilla <abnerpatrickpadilla@gmail.com>
2019-06-20 00:52:20 +0800

    Connected to db

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const ejs = require('ejs');
+const mysql = require('mysql');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-let port = 1234;
+const port = 1234;
+
+const db = mysql.createConnection ({
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'inventory'
+});
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    } console.log('Connected to database');
+});
+global.db = db;
+
+app.set('port', process.env.port || port); 
+app.set('views', __dirname + '/views'); 
+app.set('view engine', 'ejs'); 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); 
+app.use(express.static(path.join(__dirname, 'public'))); 
 
 app.listen(port, () => {
-    console.log('Server is up and running on port number ' + port);
-})
-;
+    console.log('Server is up and running on port: ' +port);
+});
